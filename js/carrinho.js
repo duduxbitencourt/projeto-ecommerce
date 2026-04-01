c8c9b3cbff6b91119456dbfe71d0218a44b2759f
Eduardo Bitencourt <bitencourtdudu88@gmail.com>
2026-04-01 08:43:20 -0300

    adiciona listener para capturar dados do produto ao clicar no carrinho

--- js/carrinho.js
+++ js/carrinho.js
@@ -1,2 +1,12 @@
-const btnAddCarrinho = document.querySelectorAll('.btn-carrinho');
+const btnAddCarrinho = document.querySelectorAll(".btn-carrinho");
 
+btnAddCarrinho.forEach((btn) => {
+  btn.addEventListener("click", (event) => {
+    const elementoProduto = event.target.closest (".produto");
+    const produtoId = elementoProduto.dataset.id;
+    const produtoNome = elementoProduto.querySelector(".nome").textContent;
+    const produtoImg = elementoProduto.querySelector("img").getAttribute("src");
+    const produtoPreco = parseFloat(elementoProduto.querySelector(".price").textContent.replace("R$ ", "").replace(".", "").replace(",", "."));
+
+  });
+});
